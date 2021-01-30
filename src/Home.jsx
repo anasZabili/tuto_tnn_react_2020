9f7672e398216f790c8be44b1e280019e440a888
anas zabili <anaszab78@gmail.com>
2021-01-30 23:35:46 +0100

    lesson 15

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,43 @@
-import { useState } from "react"
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 const Home = () => {
-  const [name, setName ] = useState("mario")
-  const [age, setAge] = useState(25)
-  const handleClick = () => {
-    setName("luigi")
-    setAge(30)
-};
+  const [blogs, setBlogs] = useState([
+    {
+      title: "Super evenement",
+      body: "creme bruler ce soir",
+      author: "Anas",
+      id: 1,
+    },
+    {
+      title: "Mauvaise nouvelle",
+      body: "plus de creme bruler ce soir",
+      author: "Anis",
+      id: 2,
+    },
+    {
+      title: "Re mauvaise nouvelle",
+      body: "on a retrouvé de la creme bruler ce soir",
+      author: "Anas",
+      id: 3,
+    },
+  ]);
+  const [name, setName] = useState("mario");
+  useEffect(() => {
+    console.log(name);
+  }, [name]);
+  const handleOnDelete = (id) => {
+    const newBlog = blogs.filter((value) => value.id !== id);
+    setBlogs(newBlog);
+  };
   return (
     <div className="home">
-      <h2>HomePage</h2>
-      <p>{name} is {age}</p>
-      <button onClick={handleClick}>Click me</button>
+      <BlogList
+        blogs={blogs}
+        title="All blogs"
+        handleOnDelete={handleOnDelete}
+      />
+      <button onClick={() => setName("luigi")}>change name</button>
+      <p> {name}</p>
     </div>
   );
 };
