eccfd226200fdc1f2e539a77b12a9b5b5603c975
anas zabili <anaszab78@gmail.com>
2021-01-31 22:21:52 +0100

    lesson 20 catch error (important)

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,31 +2,36 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
       .then((res) => {
+        // si le serveur ne renvoie pas un reponse positive je throw une erreur
+        if (!res.ok) {
+          throw Error("could not fetch data");
+        }
         return res.json();
       })
       .then((data) => {
         console.log(data);
+        setError(null);
         setBlogs(data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        // ici on catch l'erreur si on arrive pas a atteindre le server
+        setError(err.message);
+        setIsLoading(false);
       });
   }, []);
 
-  const handleOnDelete = (id) => {
-    const newBlog = blogs.filter((value) => value.id !== id);
-    setBlogs(newBlog);
-  };
   return (
     <div className="home">
-      {blogs && (
-        <BlogList
-          blogs={blogs}
-          title="All blogs"
-          handleOnDelete={handleOnDelete}
-        />
-      )}
+      {error && <div>{error}</div>}
+      {isLoading && <div>Loadong ...</div>}
+      {blogs && <BlogList blogs={blogs} title="All blogs" />}
     </div>
   );
 };
