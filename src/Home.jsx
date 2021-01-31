0d83ce5822a3ae34724520a8d82ff0f0f166aaef
anas zabili <anaszab78@gmail.com>
2021-01-31 21:52:10 +0100

    lesson 17

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,43 +1,32 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      title: "Super evenement",
-      body: "creme bruler ce soir",
-      author: "Anas",
-      id: 1,
-    },
-    {
-      title: "Mauvaise nouvelle",
-      body: "plus de creme bruler ce soir",
-      author: "Anis",
-      id: 2,
-    },
-    {
-      title: "Re mauvaise nouvelle",
-      body: "on a retrouvé de la creme bruler ce soir",
-      author: "Anas",
-      id: 3,
-    },
-  ]);
-  const [name, setName] = useState("mario");
+  const [blogs, setBlogs] = useState(null);
+
   useEffect(() => {
-    console.log(name);
-  }, [name]);
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+      });
+  }, []);
+
   const handleOnDelete = (id) => {
     const newBlog = blogs.filter((value) => value.id !== id);
     setBlogs(newBlog);
   };
   return (
     <div className="home">
-      <BlogList
-        blogs={blogs}
-        title="All blogs"
-        handleOnDelete={handleOnDelete}
-      />
-      <button onClick={() => setName("luigi")}>change name</button>
-      <p> {name}</p>
+      {blogs && (
+        <BlogList
+          blogs={blogs}
+          title="All blogs"
+          handleOnDelete={handleOnDelete}
+        />
+      )}
     </div>
   );
 };
