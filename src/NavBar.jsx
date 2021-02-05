552a4c0800d726c9b29faa8ac6165ca207ed3bf2
anas zabili <anaszab78@gmail.com>
2021-02-05 22:58:27 +0100

    lesson 23

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
-  return ( 
+  return (
     <nav className="navbar">
       <h1>Mon Blog</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a href="/create">New Blog</a>
+        <Link to="/">Home</Link>
+        <Link to="/create">New Blog</Link>
       </div>
-    </nav>  
+    </nav>
   );
-}
- 
+};
+
 export default NavBar;
